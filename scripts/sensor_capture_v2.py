14118e33dd51c5fc02161fbf8fab6d9f1f83fb1a
harsha5 <harshbitsp@gmail.com>
2019-01-13 13:30:44 -0500

    Useless files transferred to shunned edits. Sensor_capture_v2.py added

--- scripts/sensor_capture_v2.py
+++ scripts/sensor_capture_v2.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 """
-This script is used to capture sensor data from all the 7 sensors (or actuators)- Decawave UWM, 360 Fish-eye (3 cameras), Downward facing fish-eye, Intel Real Sense, Hebi Motor angle
+This script is used to capture sensor data from all the 7 sensors (or actuators)- Decawave UWM, 360 Fish-eye (3 cameras), Downward facing fish-eye, Intel Real Sense, Hebi Motor angle using joystick button
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Harsh Sharma"
 __email__ = "harshsha@cs.cmu.edu"
-__website__ = "http://mrsdprojects.ri.cmu.edu/2018teami/"
+__website__ = "http://mrsdprojects.ri.cmu.edu/2019teami/"
 __copyright__ = "Copyright (C) 2019, the H#SH Robotics. All rights reserved."
 
 import rospy
 import tf
+import sys
+import os
 from geometry_msgs.msg import PoseStamped
 import hebi
 import time
@@ -23,7 +25,6 @@ from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
 import cv2
 import csv
-
 
 
 def open_csv(filio):
@@ -60,12 +61,35 @@ def get_msg(topic_name,data_type,name_folder,count):
         #rospy.sleep(1)
 
     return [time,image_name]
+
+def make_folders(my_argv):
+	n=my_argv[1]
+	if('RS' in my_argv):
+		print("Real Sense detected")
+		path = "/home/ashish/data/real-sense{0}".format(n)
+		create_directory('RS',path)
+		RS_FOLDER=path
+	if('PG' in my_argv):
+		print("Point Grey detected")
+		path = "/home/ashish/data/PG{0}".format(n)
+		print(path)
+		create_directory('PG',path)
+		PG_FOLDER=path
+
+def create_directory(stro,path):
+		try:  
+    			os.mkdir(path)
+		except OSError:  
+    			print (stro," directory already exists")	
+			exit()	
+
 	
     
-def sensor_capture():
+def sensor_capture(my_argv):
     rospy.init_node('sensor_capture', anonymous=True)
     open_csv('sensor_data.csv')
     count=0
+    make_folders(my_argv)
     with open('sensor_data.csv','a') as file_1:
 	wr = csv.writer(file_1)
     
@@ -74,28 +98,32 @@ def sensor_capture():
 		msg = rospy.wait_for_message("ak1/joy", Joy)
 		print("Sensor Capture file running..... ", int(msg.axes[7]))
 		if(int(msg.axes[7])==1):
+			data_list=[]
 			count+=1
+			data_list.append(count)
 			print("Button Snapped")
-
-	    		'''msg_hebi=get_msg('motor_para', PoseStamped)
-	    		msg_real_sense=get_msg('/camera/color/image_raw', Image, '/home/ashish/data/real-sense/')
-			msg_decawave=get_msg('/range', Range)
-			msg_360_fish_0=get_msg('/usb_cam0/image_raw', Image, '/home/ashish/data/360-fish-0/')
-			msg_360_fish_1=get_msg('/usb_cam1/image_raw', Image, '/home/ashish/data/360-fish-1/')
-			msg_360_fish_2=get_msg('/usb_cam2/image_raw', Image, '/home/ashish/data/360-fish-2/')
-			msg_down_fish=get_msg('/camera/image_color', Image, '/home/ashish/data/down-fish/')
-			data_list=[count,msg_hebi.header.stamp,msg_header.pose.orientation.x,msg_header.pose.orientation.y,msg_real_sense[0],msg_real_sense[1],msg_decawave.header.stamp,msg_decawave.range,msg_360_fish_0[0],msg_360_fish_0[1],msg_360_fish_1[0],msg_360_fish_1[1],msg_360_fish_2[0],msg_360_fish_2[1],msg_down_fish[0],msg_down_fish[1]]'''
+			if('DW' in my_argv):
+				msg_decawave=get_msg_twoargs('/range', Range)
+				data_list.append(msg_decawave.header.stamp)
+				data_list.append(msg_decawave.range)
+				print("Decawave captured")
 			
+			if('HEBI' in my_argv):
+				msg_hebi=get_msgtwoargs('motor_para', PoseStamped)
+				data_list.append(msg_hebi.header.stamp)
+				data_list.append(msg_header.pose.orientation.x)
+				data_list.append(msg_header.pose.orientation.y)
 
-			msg_decawave=get_msg_twoargs('/range', Range)
-			print("Decawave captured")
+			if('RS' in my_argv):
+				msg_real_sense=get_msg('/camera/color/image_raw', Image, RS_FOLDER,count)
+				data_list.append(msg_real_sense[0])
+				data_list.append(msg_real_sense[1])
 			
-			msg_down_fish=get_msg('/pg_camera/image_color', Image, '/home/ashish/data/down-fish/',count)
-			print("Downward fish-eye data obtained")
-			#msg_360_fish_2=get_msg('/usb_cam2/image_raw', Image, '/home/ashish/data/360-fish-2/',count)
-			#data_list=[str(msg_real_sense[0]),str(msg_real_sense[1]),msg_360_fish_2[0],str(msg_360_fish_2[1])]
-			#data_list=[msg_real_sense[0],msg_real_sense[1],msg_360_fish_2[0],msg_360_fish_2[1]]
-			data_list=[count,msg_decawave.header.stamp,msg_decawave.range,msg_down_fish[0],msg_down_fish[1]]
+			if('PG' in my_argv):
+				msg_down_fish=get_msg('/pg_camera/image_color', Image, PG_FOLDER,count)
+				data_list.append(msg_down_fish[0])
+				data_list.append(msg_down_fish[1])
+
 			write_csv(data_list,wr)
 	    		continue
     
@@ -103,7 +131,11 @@ def sensor_capture():
 
 
 if __name__ == '__main__':
+    PG_FOLDER=''
+    RS_FOLDER=''
+    my_argv = rospy.myargv(argv=sys.argv)
+    print(my_argv)
     try:
-        sensor_capture()
+        sensor_capture(my_argv)
     except rospy.ROSInterruptException:
         pass
