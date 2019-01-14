95dcd8846729cdc89590e4335907bf5e42fbffcc
harsha5 <harshbitsp@gmail.com>
2019-01-13 23:39:48 -0500

    Original motor positions added, instead of stepo

--- scripts/hebi_joy_v2.py
+++ scripts/hebi_joy_v2.py
@@ -7,28 +7,19 @@ using kinematic control approach.
 """
 
 __version__ = "0.0.1"
-__author__ = "David Qiu"
-__email__ = "dq@cs.cmu.edu"
-__website__ = "http://mrsdprojects.ri.cmu.edu/2017teami/"
-__copyright__ = "Copyright (C) 2018, the Moon Wreckers. All rights reserved."
+__author__ = "Harsh Sharma"
+__email__ = "harshsha@cs.cmu.edu"
+__website__ = "http://mrsdprojects.ri.cmu.edu/2019teami/"
+__copyright__ = "Copyright (C) 2019, the H#SH Robotics. All rights reserved."
 
 import rospy
 import tf
-# from std_msgs.msg import String, Float32, UInt16
-# from sensor_msgs.msg import Imu, JointState, Joy
-# from geometry_msgs.msg import Vector3Stamped, Twist
-# from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
 import hebi
 import time
 import numpy as np
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String
-
-VERT_MOTOR_OFFSET = .09  # cm
-HORZ_MOTOR_OFFSET = .07  # cm
-CLAW_ARM_LENGTH = .31  # cm
-#stepo=0
 
 def auto_pointing():
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -41,7 +32,8 @@ def auto_pointing():
     hebi_modules = [('arm', 'joint_0'), ('arm', 'joint_1')]
 
     theta_bounds = [[-.7, 1.4], [-0.8, 0.8]]
-    # motion_bound = 0.05
+    #theta_bounds = [-.1.32, 0.8]
+
 
     # discover HEBI modules
     count_found = 0
@@ -65,7 +57,6 @@ def auto_pointing():
     group_command = hebi.GroupCommand(hebi_group.size)
 
     #PUBLISH ON TOPIC
-    		#pub = rospy.Publisher('motor_para', String, queue_size=10)
     pub = rospy.Publisher('motor_para', PoseStamped, queue_size=10)
     rate = rospy.Rate(30) # 30hz
     goal = PoseStamped()
@@ -76,7 +67,7 @@ def auto_pointing():
     # initialize HEBI actuator position control parameters
     # update_alpha = 0.5
     theta = np.array([0.0, 0.0])
-    stepo = 0
+    stepo = 0.8
     rate = rospy.Rate(20)
     #rospy.Subscriber("joy", Joy, callback)
     while not rospy.is_shutdown():
@@ -85,39 +76,31 @@ def auto_pointing():
 	msg = rospy.wait_for_message("ak2/joy", Joy)
 	stepo=stepo+.02*msg.axes[6]
     	theta=np.array([stepo, 0.0])
-	#motor_angle=str(theta[0])+' '+str(theta[1])
+
+	# Fill in feedback
+	group_feedback = hebi.GroupFeedback(hebi_group.size)
+	group_feedback = hebi_group.get_next_feedback(reuse_fbk=group_feedback)
+
+	# Retrieve positions:
+	positions = group_feedback.position
+
 	goal.header.stamp=rospy.Time.now()
-	goal.pose.orientation.x=theta[0] #This is the pan of the hebi motor
-	goal.pose.orientation.y=theta[1] #This is the tilt of the hebi motor
+	goal.pose.orientation.x=positions[0] #This is the pan of the hebi motor
+	goal.pose.orientation.y=positions[1] #This is the tilt of the hebi motor
 	pub.publish(goal)
-	#pub.publish(motor_angle)
+
 
     # send command to HEBI control group
         group_command.position = theta
         hebi_group.send_command(group_command)
-	time.sleep(.2)
-	
-        #print('theta1 = %.4f; %.4f; %.4f, theta2 = %.4f; %.4f; %.4f, x_c = (%.2f, %.2f, %.2f)' % (
-            #theta[0],theta_actual[0],180 * theta[0] / np.pi,theta[1],theta_actual[1], 180 * theta[1] / np.pi, x, y, z))
-	
-       	print('theta1 = %.4f, theta2 = %.4f; stepo= %.4f;' % (theta[0],theta[1],stepo))
+	time.sleep(.2)	
+	print('Position Feedback:\nmotor_actual_position: {0}\tstepo: {1}\t'.format(positions,stepo))
         rate.sleep()
 
     #rospy.Subscriber("ak2/joy", Joy, callback)
 
 
     # rospy.spin() # use spin only in pure event handling programming model
-
-'''def callback(data):
-    print(data.axes[6])
-    stepo += .1*data.axes[6]
-    theta=np.array([stepo, 0.0])
-    # send command to HEBI control group
-    group_command.position = theta
-    hebi_group.send_command(group_command)
-    time.sleep(.2)
-    print('theta1 = %.4f, theta2 = %.4f; stepo= %.4f;' % (theta[0],theta[1],stepo))'''
-
 
 if __name__ == '__main__':
     try:
